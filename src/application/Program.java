b10a68a9de8372101fe8e302b730a46aa7450403
DiegoVP66 <dvpdev33@gmail.com>
2021-05-31 23:34:30 -0300

    Retrieving data from the database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import dataB.DataBConnection;
 
@@ -8,9 +11,36 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		// testing database connection and disconnection
-		Connection connect = DataBConnection.getConnection();
-		DataBConnection.closeConnection();
+		
+		Connection connect = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		// Retrieving data from the database
+		try {
+			connect = DataBConnection.getConnection();
+			
+			statement = connect.createStatement();
+                                             
+			resultSet = statement.executeQuery("select * from seller");
+			
+			while(resultSet.next()) {
+				
+				System.out.println(resultSet.getInt("Id")+ " - "
+						+ resultSet.getString("Name")
+						+", "
+						+resultSet.getString("Email"));
+			}
+			
+			
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DataBConnection.closeConnection();
+		}
 
 	}
 
