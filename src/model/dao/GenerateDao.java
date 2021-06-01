d81dd85f5b2295a9ebb5b46a211142477a278c8b
DiegoVP66 <dvpdev33@gmail.com>
2021-06-01 19:36:36 -0300

    / implementing the sql code in the findById method

--- src/model/dao/GenerateDao.java
+++ src/model/dao/GenerateDao.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import dataB.DataBConnection;
 import model.dao.impl.SellerDaoJDBC;
 
 public class GenerateDao {
 	// returns  Interface SellerDao
 	public static SellerDao generateSellerDao() {
 		// protects the implementation without exposing it
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DataBConnection.getConnection());
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import dataB.DataBConnection;
+import dbExceptions.DataBException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+	
+	private  Connection connect;
+	
+	// creating dependency between connection and class
+	public  SellerDaoJDBC(Connection connect) {
+		this.connect = connect;
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -27,6 +41,45 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			connect = DataBConnection.getConnection();
+			st = connect.prepareStatement(
+					 "SELECT seller.*,department.Name as DepName "
+					 + "FROM seller INNER JOIN department "
+					 + "ON seller.DepartmentId = department.Id "
+					 + "WHERE seller.id = ?");
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			if(rs.next()) {
+				// binding the obj department to the database department
+				Department dep = new Department();
+				dep.setDepId(rs.getInt("DepartmentId"));
+				dep.setDepName(rs.getString("DepName"));
+				
+				// binding the obj seller to the database seller
+				Seller seller = new Seller();
+				seller.setId(rs.getInt("Id"));
+				seller.setName(rs.getString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBirthDate(rs.getDate("BirthDate"));
+				seller.setBaseSalary(rs.getDouble("BaseSalary"));
+				// binding the obj seller to the obj department 
+				seller.setDepartmentId(dep);
+				return seller;
+			}
+		}
+		catch(SQLException e) {
+			throw new DataBException(e.getMessage());
+		}
+		finally {
+			DataBConnection.closeResultSet(rs);
+			DataBConnection.closeStatement(st);
+		}
+		
 		
 		return null;
 	}
