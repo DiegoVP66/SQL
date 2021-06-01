c68dcdae31486edc3e21e8ba491b6a297626458e
DiegoVP66 <dvpdev33@gmail.com>
2021-05-31 23:10:29 -0300

    method to connect to database

--- src/dataB/DataBConnection.java
+++ src/dataB/DataBConnection.java
@@ -2,24 +2,46 @@ package dataB;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import dbExceptions.DataBException;
 
 public class DataBConnection {
-	
-	
+
+	private static Connection connect = null;
+
+	// method to connect to database
+	public static Connection getConnection() {
+		if (connect == null) {
+			try {
+				Properties properties = loadDBProperties();
+
+				String url = properties.getProperty("dburl");
+
+				connect = DriverManager.getConnection(url);
+				
+			} catch (SQLException e) {
+				throw new DataBException(e.getMessage());
+			}
+		}
+		return connect;
+	}
+
 	// method to read properties
 	private static Properties loadDBProperties() {
-		try(FileInputStream fileInput =  new FileInputStream("db.properties")){
-			
+		
+		try (FileInputStream fileInput = new FileInputStream("db.properties")) {
+
 			Properties properties = new Properties();
-			
+
 			properties.load(fileInput);
-			
+
 			return properties;
-		}
-		catch(IOException e) {
+			
+		} catch (IOException e) {
 			throw new DataBException(e.getMessage());
 		}
 	}
