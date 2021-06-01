d896ce443c5f810e09e517f03877e22a6880a4d1
DiegoVP66 <dvpdev33@gmail.com>
2021-06-01 16:13:02 -0300

    Department class, package models.entities

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,60 +1,11 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import dataB.DataBConnection;
-import dbExceptions.DataBException;
-
 public class Program {
 
 	public static void main(String[] args) {
-	
-		
-		Connection connect = null;
-		Statement statement = null;
 		
 		
 		
-		try {
-			connect = DataBConnection.getConnection();
-			
-			// protecting the transaction
-			connect.setAutoCommit(false);
-			
-			statement = connect.createStatement();
-				                              
-			int lines = statement.executeUpdate("UPDATE seller SET BaseSalary = 4000 WHERE Id = 1");
-			
-			
-			int lines1 = statement.executeUpdate("UPDATE seller SET BaseSalary = 6000 WHERE Id = 2");
-			
-			// showing how many lines were affected
-			System.out.println("UPDATED DATA, Lines affected: " +lines);
-			System.out.println("UPDATED DATA, Lines affected: " +lines1);
-			
-			// protecting the transaction
-			connect.commit();
-			
-		}
-		catch(SQLException e) {
-			try {
-				// if the transaction fails
-				connect.rollback();
-				throw new DataBException("Transaction failed! Error: "+e.getMessage());
-			} catch (SQLException e1) {
-				
-				throw new DataBException("Error: "+e1.getMessage());
-			}
-		}
-		
-		finally {
-			
-			DataBConnection.closeStatement(statement);
-			DataBConnection.closeConnection();
-		}
-
 	}
 
 }
---
+++ src/models/entities/Department.java
@@ -0,0 +1,67 @@
+package models.entities;
+
+import java.io.Serializable;
+
+public class Department implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer depId;
+	private String depName;
+
+	public Department() {
+	}
+
+	public Department(Integer depId, String depName) {
+		this.depId = depId;
+		this.depName = depName;
+	}
+
+	public Integer getDepId() {
+		return depId;
+	}
+
+	public void setDepId(Integer depId) {
+		this.depId = depId;
+	}
+
+	public String getDepName() {
+		return depName;
+	}
+
+	public void setDepName(String depName) {
+		this.depName = depName;
+	}
+
+	// hashcode and equals to compare items by content
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((depId == null) ? 0 : depId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (depId == null) {
+			if (other.depId != null)
+				return false;
+		} else if (!depId.equals(other.depId))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [depId=" + depId + ", depName=" + depName + "]";
+	}
+
+}
