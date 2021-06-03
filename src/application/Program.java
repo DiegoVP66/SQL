5d3804eb9a0ab445f0c3e356aeaa63a7def57510
DiegoVP66 <dvpdev33@gmail.com>
2021-06-03 19:23:11 -0300

    DepartmentDaoJDBC and all implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,79 +1,44 @@
 package application;
 
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
-
 import dataB.DataBConnection;
+import model.dao.DepartmentDao;
 import model.dao.GenerateDao;
-import model.dao.SellerDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
-		Locale.setDefault(Locale.US);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Scanner sc =  new Scanner(System.in);
-		SellerDao sellerDao = GenerateDao.generateSellerDao();
+	public static void main(String[] args) {
 
-		// testing findById method on main class
-		System.out.println("findById");
-		Seller seller = sellerDao.findById(1);
-		System.out.println(seller);
+		DepartmentDao depDao = GenerateDao.generateDepartmentDao();
+		// findById
+		Department dep = depDao.findById(1);
+		System.out.println("Before Update");
+		System.out.println(dep);
 		
 		System.out.println();
 		
-		// testing findByDepartment method on main class
-		Department dep = new Department(1, null);
-		List<Seller> list = sellerDao.findByDepartment(dep);
-		System.out.println("findByDepartment");
+		// findAll
+		//List<Department> list = depDao.findAll();
 		
-		//Stream.of(list).forEach(System.out::println);
-		
-		for(Seller x : list) {
+		/*for(Department x : list){
 			System.out.println(x);
-		}
+		}*/
 		
-		// testing findAll
-		list = sellerDao.findAll();
-		System.out.println("FindAll");
-		for(Seller y : list) {
-			System.out.println(y);
-		}
+		// insert
+		//Department d = new Department(null, "Music");
+		//depDao.insert(d);
 		
-		System.out.println();
+		// Update
+		dep.setDepName("Development");
+		depDao.update(dep);
+		System.out.println("After Update");
+		System.out.println(dep);
 		
-		// testing insert
-		/*System.out.print("Name: ");
-		String name = sc.nextLine();
-		System.out.print("Email: ");
-		String email = sc.nextLine();
-		System.out.print("Birth date: ");
-		Date date = sdf.parse(sc.next());
-		System.out.print("Base salary: ");
-		Double salary = sc.nextDouble();
 		
-		seller = new Seller(null,name, email, date, salary, dep);
-		sellerDao.insert(seller);
-		
-		System.out.println("id: "+seller.getId());*/
-		
-		// testing update
-		//seller =  sellerDao.findById(16);
-		//seller.setBaseSalary(4000.00);
-		//sellerDao.update(seller);
-		sellerDao.deleteById(17);
-		
+		// delete
+		depDao.deleteById(11);
 		
 		DataBConnection.closeConnection();
-		sc.close();
-
 	}
 
 }
--- src/model/dao/GenerateDao.java
+++ src/model/dao/GenerateDao.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import dataB.DataBConnection;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class GenerateDao {
@@ -8,5 +9,9 @@ public class GenerateDao {
 	public static SellerDao generateSellerDao() {
 		// protects the implementation without exposing it
 		return new SellerDaoJDBC(DataBConnection.getConnection());
+	}
+	
+	public static DepartmentDao generateDepartmentDao(){
+		return new DepartmentDaoJDBC(DataBConnection.getConnection());
 	}
 }
